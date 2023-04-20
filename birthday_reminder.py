f58caf7fd28cf8087026239d05f6cc17a77337e5
amuracciole <amuracciole@gmail.com>
2023-04-20 11:33:50 +0200

    improves telegram msg

--- birthday_reminder.py
+++ birthday_reminder.py
@@ -10,8 +10,12 @@ def check_birthday():
             print(f"Today is {name}'s birthday!")
             name_list.append(name)
     string_name_list = "\n".join(name_list)
-    print(string_name_list)
-    send_telegram_msg(today, string_name_list)
+    print("LISTA:")
+    #print(string_name_list,len(string_name_list))
+    if len(string_name_list) == 0:
+        exit()
+    else:
+        send_telegram_msg(today, string_name_list)
     
 ### PROGRAM ###
 check_birthday()
