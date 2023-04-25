bc75a315befbd4f4f8864683d94f13ea1775a645
amuracciole <amuracciole@gmail.com>
2023-04-25 16:09:10 +0200

    one day before reminder and fix issue

--- reminder.py
+++ reminder.py
@@ -3,24 +3,28 @@ from birthdays import birthdays_list
 from events import events_list
 from telegram_bot import *
 
-def check_birthday():
-    today = datetime.date.today().strftime('%d/%m')
-    complete_today = datetime.date.today().strftime('%d/%m/%Y')
+def check_birthday(when):
+    if when == "Today":
+        whan_date = datetime.date.today().strftime('%d/%m')
+        complete_when_date = datetime.date.today().strftime('%d/%m/%Y')
+    elif when == "Tomorrow":
+        whan_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m')
+        complete_when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
     name_list=[]
     count_list=[]
     for name, date in birthdays_list.items():
-        if date[:5] == today:
+        if date[:5] == whan_date:
             count=str(int(datetime.date.today().strftime('%Y'))-int(date[6:]))
-            print(f"Today is {name}'s birthday ({count})!")
+            print(f"{when} is {name}'s birthday ({count})!")
             name_list.append(name)
             count_list.append(count)
-    #print(string_name_list,len(string_name_list))
     if len(name_list) == 0:
-        exit()
+        return()
     else:
-        send_telegram_msg(complete_today, name_list, count_list)
+        send_telegram_msg(when, complete_when_date, name_list, count_list)
 
 def check_events():
+    print("events")
     today = datetime.date.today().strftime('%d/%m')
     complete_today = datetime.date.today().strftime('%d/%m/%Y')
     event_list=[]
@@ -29,12 +33,13 @@ def check_events():
             print(f"Reminder for today: {name}!")
             event_list.append(name)
     string_event_list = "\n".join(event_list)
-    #print(string_name_list,len(string_name_list))
     if len(string_event_list) == 0:
-        exit()
+        return
     else:
         send_telegram_msg(complete_today, string_event_list)   
 
+
 ### PROGRAM ###
-check_birthday()
+check_birthday("Today")
+check_birthday("Tomorrow")
 check_events()
--- telegram_bot.py
+++ telegram_bot.py
@@ -4,10 +4,10 @@ import config
 
 # https://www.youtube.com/watch?v=M9IGRWFX_1w&ab_channel=BaoLuThe
 
-def send_telegram_msg(today, names, count):
+def send_telegram_msg(when, date, names, count):
     token=config.TELEGRAM_TOKEN
     chat_id = config.TELEGRAM_CHAT_ID
-    text_1 = "🎂 Today ({}) is the birthday of:".format(today)
+    text_1 = "🎂 {} ({}) is the birthday of:".format(when, date)
     text_2 = "\n"
     for x in range(0,len(count)):
         text_2 = text_2 + "{} ({})".format(names[x], count[x]) + "\n"
