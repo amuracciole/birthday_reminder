e0ba68133eba37ce4946d42fc48c3ea41258807b
amuracciole <amuracciole@gmail.com>
2023-05-29 20:09:58 +0200

    improve from list to json

--- menu.py
+++ menu.py
@@ -3,137 +3,87 @@
 import datetime
 import os
 import time
-from birthdays import birthdays_list
-from events import events_list
+import json
 
 def main_menu():
     print("1 - Add event")
     print("2 - Delete event")
     print("3 - See event")
     print("0 - EXIT\n")
-    
-def add_event_menu():
+  
+def select_type_menu():
     print("1 - Add birthday")
     print("2 - Add other event")
     print("0 - EXIT\n")
-    
-def delete_event_menu():
-    print("1 - Delete birthday")
-    print("2 - Delete other event")
-    print("0 - EXIT\n")
 
-def see_event_menu():
-    print("1 - See birthdays")
-    print("2 - See other events")
-    print("0 - EXIT\n")
-    
+def add_value(name, type, date, recurrency):
+    with open('events.json', encoding='utf-8') as file:
+        data = json.load(file)
+    new_event = {
+        "name": name,
+        "type": type,
+        "date": date,
+        "recurrency": recurrency
+    }
+    data['events'].append(new_event)
+    with open('events.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
 ### PROGRAM ###
 while (True):
     main_menu()
     main_option = input("OPTION: ")
     if (main_option == "1"):
         os.system('clear')
-        add_event_menu()
-        option = input("OPTION: ")
-        if option == "1":
-            name = input("Name of the person: ")
-            date = input("Enter the birthday in the format DDMMYYYY: ")
-            try:
-                date = datetime.datetime.strptime(date, "%d%m%Y")
-                birthdays_list[name] = date.strftime("%d/%m/%Y")
-                with open('birthdays.py', 'w') as f:
-                    f.write('birthdays_list = ' + str(birthdays_list))
-                print("Added successfully!")
-            except ValueError:
-                print("Invalid date format. Try again.")
-                break
-            time.sleep(1)
-            os.system('clear')
-        elif option == "2":
-            event_name = input("Name of the event: ")
-            event_date = input("Enter the date in the format DDMM: ")
-            try:
-                event_date = datetime.datetime.strptime(event_date, "%d%m")
-                events_list[event_name] = event_date.strftime("%d/%m")
-                with open('events.py', 'w') as f:
-                    f.write('events_list = ' + str(events_list))
-                print("Added successfully!")
-            except ValueError:
-                print("Invalid date format. Try again.")
-                break
-            time.sleep(1)
-            os.system('clear')
-        elif option =="0":
-            os.system('clear')
-            break  
+        type = "Event"
+        name = input("Name of the event: ")
+        date = input("Enter the date in the format DDMMYYYY: ")
+        type = input("Birthday (B) or Other (O): ")
+        if (type == "B" or type == "b"):
+            type = "Birthday"
+        elif (type == "O" or type == "o"):
+            type = "Event"
+        recurrency = input("Recurrenty (Y/N): ")
+        if (recurrency == "Y" or recurrency == "y"):
+            recurrency = True
+        elif (recurrency == "N" or recurrency == "n"):
+            recurrency = False
+        try:
+            date = datetime.datetime.strptime(date, "%d%m%Y")
+            date = date.strftime("%d/%m/%Y")
+            add_value (name, type, date, recurrency)
+            print("Added successfully!")
+        except ValueError:
+            print("Invalid date format. Try again.")
+            break
+        time.sleep(1)
+        os.system('clear')
     
     elif main_option == "2":
         os.system('clear')
-        delete_event_menu()
-        option = input("OPTION: ")
-        if option == "1":
-            os.system('clear')
-            print("Select the number of the entry to delete:\n")
-            with open('birthdays.py', 'r') as f:
-                exec(f.read()) 
-            for i, name in enumerate(birthdays_list):
-                print(f"{i + 1}. {name} - {birthdays_list[name]}")
-            selection = input("Selection: ")
-            try:
-                index = int(selection) - 1
-                name_to_delete = list(birthdays_list.keys())[index]
-                birthdays_list.pop(name_to_delete)
-                with open('birthdays.py', 'w') as f:
-                    f.write('birthdays_list = ' + str(birthdays_list))
-                print(f"\nDeleted {name_to_delete}")
-            except (ValueError, IndexError):
-                print("\nInvalid selection. Try again.")
-            os.system('clear')
-        elif option == "2":
-            os.system('clear')
-            print("Select the number of the entry to delete:\n")
-            with open('events.py', 'r') as f:
-                exec(f.read()) 
-            for i, event_name in enumerate(events_list):
-                print(f"{i + 1}. {event_name} - {events_list[event_name]}")
-            selection = input("Selection: ")
-            try:
-                index = int(selection) - 1
-                event_to_delete = list(events_list.keys())[index]
-                events_list.pop(event_to_delete)
-                with open('events.py', 'w') as f:
-                    f.write('events_list = ' + str(events_list))
-                print(f"\nDeleted {event_to_delete}")
-            except (ValueError, IndexError):
-                print("\nInvalid selection. Try again.")
-            os.system('clear')
-        elif option =="0":
-            os.system('clear')
-            break  
+        print("Select the number of the entry to delete:\n")
+        with open('events.json', encoding='utf-8') as file:
+            data = json.load(file)
+        for i, event in enumerate(data['events'], start=1):
+            print(f"{i} - {json.dumps(event)}")
+        selection = input("Selection: ")
+        try:
+            del data['events'][int(selection) - 1]
+            with open('events.json', 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4)
+            print("Entrada eliminada con éxito.")
+        except (ValueError, IndexError):
+            print("\nInvalid selection. Try again.")
+        os.system('clear')
     
     elif main_option == "3":
         os.system('clear')
-        see_event_menu()
-        option = input("OPTION: ")
-        if option == "1":
-            os.system('clear')
-            with open('birthdays.py', 'r') as f:
-                exec(f.read())
-            for name, date in birthdays_list.items():
-                print(f"{name} - {date}")
-            input("\nPress enter to continue")
-            os.system('clear')
-        elif option =="2":
-            os.system('clear')
-            with open('events.py', 'r') as f:
-                exec(f.read())
-            for event_name, event_date in events_list.items():
-                print(f"{event_name} - {event_date}")
-            input("\nPress enter to continue")
-            os.system('clear')
-        elif option =="0":
-            os.system('clear')
-            break   
+        with open('events.json') as file:
+            data = json.load(file)
+        for i, event in enumerate(data['events'], start=1):
+            print(f"{i} - {json.dumps(event)}")
+        input("\nPress enter to continue")
+        os.system('clear')  
     
     elif main_option == "0":
         os.system('clear')
--- reminder.py
+++ reminder.py
@@ -1,29 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from birthdays import birthdays_list
-from events import events_list
 from telegram_bot import *
-
-def check_birthday(when):
-    if when == "Today":
-        when_date = datetime.date.today().strftime('%d/%m')
-        complete_when_date = datetime.date.today().strftime('%d/%m/%Y')
-    elif when == "Tomorrow":
-        when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m')
-        complete_when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
-    name_list=[]
-    count_list=[]
-    for name, date in birthdays_list.items():
-        if date[:5] == when_date:
-            count=str(int(datetime.date.today().strftime('%Y'))-int(date[6:]))
-            print(f"{when} is {name}'s birthday ({count})!")
-            name_list.append(name)
-            count_list.append(count)
-    if len(name_list) == 0:
-        return()
-    else:
-        send_telegram_msg(when, complete_when_date, name_list, count_list)
+import json
 
 def check_event(when):
     if when == "Today":
@@ -32,16 +11,47 @@ def check_event(when):
     elif when == "Tomorrow":
         when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m')
         complete_when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
+        
+    with open('events.json', encoding='utf-8') as file:
+        data = json.load(file)
+    
     event_list=[]
-    for event, date in events_list.items():
-        if date[:5] == when_date:
-            print(f"Reminder for today: {event}!")
-            event_list.append(event)
+    for event in data['events']:
+        if (event['date'][0:5] == when_date) and (event['type'] == "Event"):
+            if (event['date'] == complete_when_date) or ((event['date'] != complete_when_date) and (event['recurrency'] == True)):
+                print(f"Reminder for today: {event['name']}!")
+                event_list.append(event['name'])
+
     if len(event_list) == 0:
         return()
     else:
         send_telegram_msg(when, complete_when_date, event_list, "null")  
+        
+def check_birthday(when):
+    if when == "Today":
+        when_date = datetime.date.today().strftime('%d/%m')
+        complete_when_date = datetime.date.today().strftime('%d/%m/%Y')
+    elif when == "Tomorrow":
+        when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m')
+        complete_when_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
+        
+    with open('events.json', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    name_list=[]
+    count_list=[]
+    for event in data['events']:
+        if (event['date'][0:5] == when_date) and (event['type'] == "Birthday"):
+            count=str(int(datetime.date.today().strftime('%Y'))-int(event['date'][6:]))
+            print(f"{when} is {event['name']}'s birthday ({count})!")
+            name_list.append(event['name'])
+            count_list.append(count)
 
+    if len(name_list) == 0:
+        return()
+    else:
+        send_telegram_msg(when, complete_when_date, name_list, count_list)
+            
 ### PROGRAM ###
 check_birthday("Today")
 check_birthday("Tomorrow")
